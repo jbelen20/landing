8ac8c1ec4b87b5ea924912a1ba9e060ebc2d4601
jbelen20 <judithcarrenovega@gmail.com>
2025-06-27 00:08:44 -0400

    trabajandondo en el navbar, function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
 import { LogoMenu, ContainNavbar } from '../style/NavbarSc'
+import styled from 'styled-components'
 
 export default function Navbar() {
   return (
     <ContainNavbar>
+      <ButtonsContainer>
+        <Buttons>Home</Buttons>
+        <Buttons>Productos</Buttons>
+        <Buttons>Promos</Buttons>
+        <Buttons>Nosotros</Buttons>
+        <Buttons>Suscursales</Buttons>
+        <Buttons>Contacto</Buttons>
+      </ButtonsContainer>
       <LogoMenu xmlns="http://www.w3.org/2000/svg" fill="#fff" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
       </LogoMenu>
     </ContainNavbar>
   )
 }
+
+const Buttons = styled.button`
+  background: #D40404;
+  padding: 0;
+  height: 6vh;
+  color: #fff;
+  margin: 1rem;
+  padding: 0 1rem;
+  font-weight: 400;
+  `
+  const ButtonsContainer = styled.div`
+  margin-top: 0.7rem;
+  display: flex;
+  justify-content: space-between;
+  `
--- src/style/NavbarSc.js
+++ src/style/NavbarSc.js
@@ -15,7 +15,7 @@ export const ContainNavbar = styled.div`
   background: #000000;
   background: linear-gradient(180deg, rgba(0, 0, 0, 1) 71%, rgba(0, 0, 0, 0.88) 84%, rgba(0, 0, 0, 0.54) 90%, rgba(0, 0, 0, 0) 96%);
   display: flex;
-  justify-content: end;
+  justify-content: space-between;
   psition: absolute;
   z-index: 1000;
 `
